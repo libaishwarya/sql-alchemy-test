057f122193a5631f08afd0c6211931577e4ba77c
libaishwarya <libaishwarya@gmail.com>
2024-02-27 20:42:41 +0530

    implemented registeration in sqlalchemy Mysql

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request
+from model import generate_uuid, User, Address,Session
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+@app.route('/register', methods=['POST'])
+def register():
+    if request.method == 'POST':
+        data = request.json
+        u_id = generate_uuid()
+        name = data.get('name')
+        email = data.get('email')
+        address = data.get('address')
+
+        new_user = User(id=u_id,name=name, email=email)
+        user_address = Address(user_id=u_id,address=address)
+
+        session = Session()
+        session.add(new_user)
+        session.add(user_address)
+        session.commit()
+        session.close()
+    
+        return 'User registered successfully', 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,29 @@
+from sqlalchemy import ForeignKey, create_engine, Column, String
+from sqlalchemy.orm import sessionmaker,  relationship
+from sqlalchemy.ext.declarative import declarative_base
+import uuid 
+
+engine = create_engine("mysql+mysqlconnector://root:PASSWORD@localhost:3306/sqlalchemy_mysql")
+
+Session = sessionmaker(bind=engine)
+
+def generate_uuid():
+    return str(uuid.uuid4())
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'userTable'
+    id = Column(String(225), primary_key=True, default=generate_uuid)
+    name = Column(String(50))
+    email = Column(String(120))  # Changed to String(120)
+    addresses = relationship("Address",back_populates = "users")
+
+class Address(Base):
+    __tablename__ = 'userAddress'
+    id = Column(String(225), primary_key=True, default=generate_uuid)
+    user_id = Column(String(120), ForeignKey("userTable.id"))
+    address = Column(String(225))
+    users = relationship("User",back_populates="addresses")
+
+Base.metadata.create_all(engine)
