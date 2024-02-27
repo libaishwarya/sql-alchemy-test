890b56f00807f83018e636498a9c6cdf5174ee35
libaishwarya <libaishwarya@gmail.com>
2024-02-27 22:38:23 +0530

    init commit - user table added

---
+++ user_product_management/main.py
@@ -0,0 +1,22 @@
+from sqlalchemy import ForeignKey, create_engine, Column, String
+from sqlalchemy.orm import sessionmaker,  relationship
+from sqlalchemy.ext.declarative import declarative_base
+import uuid 
+
+engine = create_engine("mysql+mysqlconnector://root:PASSWORD@localhost:3306/user_product")
+
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+def generate_uuid():
+    return str(uuid.uuid4())
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(String(225), primary_key=True, default=generate_uuid)
+    name = Column(String(50))
+    email = Column(String(120))  # Changed to String(120)
+    addresses = relationship("Address",back_populates = "users")
+
+
+Base.metadata.create_all(engine)
