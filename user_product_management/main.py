2a5dd54d97a85bfd3e70cc0a65cf748a7e0e434f
libaishwarya <libaishwarya@gmail.com>
2024-02-28 11:29:43 +0530

    added the relationship and joins in sqlalchemy

--- user_product_management/main.py
+++ user_product_management/main.py
@@ -1,9 +1,10 @@
+from psutil import users
 from sqlalchemy import ForeignKey, create_engine, Column, String, Integer, select
 from typing import List
 from sqlalchemy.orm import sessionmaker,  relationship,Mapped, mapped_column, Session
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
-
+from sqlalchemy.dialects import mysql
 
 engine = create_engine("mysql+mysqlconnector://root:PASSWORD@localhost:3306/user_product")
 
@@ -18,19 +19,25 @@ class User(Base):
     id:Mapped[str] = mapped_column(String(100),primary_key=True, default=generate_uuid)
     name:Mapped[str] = mapped_column(String(100))
     email:Mapped[str] = mapped_column(String(100))
+    u_product:Mapped[List["UserProducts"]]= relationship(back_populates="user")
 
 class Product(Base):
     __tablename__ = 'product'
     id:Mapped[str] = mapped_column(String(100),primary_key=True, default=generate_uuid)
     name:Mapped[str] = mapped_column(String(100))
     price:Mapped[int] = mapped_column(String(100))
+    product:Mapped[List["UserProducts"]]= relationship(back_populates="product_details")
+
     
 class UserProducts(Base):
     __tablename__ = "user_products"
     id:Mapped[str] = mapped_column(String(100),primary_key=True, default=generate_uuid)
     user_id:Mapped[str] = mapped_column(String(100),ForeignKey("user.id"))
     product_id:Mapped[str] = mapped_column(String(100),ForeignKey("product.id"))
+    user:Mapped["User"]= relationship(back_populates= "u_product")
+    product_details:Mapped["Product"]= relationship(back_populates="product")
 
+Base.metadata.create_all(engine)
 
 u_id = generate_uuid()
 p_id = generate_uuid()
@@ -53,5 +60,13 @@ s.add_all([u, p, up])
 s.commit()
 print(u)
 
+stmt = select(User).join(UserProducts.user).join(UserProducts.product_details)
+print(stmt.compile(dialect=mysql.dialect()))
+data = s.scalars(stmt).all()
+print(data)
 
-Base.metadata.create_all(engine)
+for r in data:
+    for product in r.u_product:
+        print(r.id,r.name, r.email,product.id, product.product_id,product.product_details.name )
+
+# Base.metadata.create_all(engine)
